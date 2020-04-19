465e7e2a55b4dfdff65ee716d13e262d29a49829
unknown <abhivispute33@gmail.com>
2020-04-20 03:59:40 +0530

    dashbaord

--- base/blockchain/index.js
+++ base/blockchain/index.js
@@ -1,4 +1,5 @@
-const Block = require('./block');
+const Block = require("./block");
+const request = require("request");
 
 class Blockchain {
   constructor({ state }) {
@@ -9,29 +10,67 @@ class Blockchain {
   addBlock({ block, transactionQueue }) {
     return new Promise((resolve, reject) => {
       Block.validateBlock({
-        lastBlock: this.chain[this.chain.length-1],
+        lastBlock: this.chain[this.chain.length - 1],
         block,
-        state: this.state
-      }).then(() => {
-        this.chain.push(block);
+        state: this.state,
+      })
+        .then(() => {
+          this.chain.push(block);
 
-        Block.runBlock({ block, state: this.state });
+          Block.runBlock({ block, state: this.state });
 
-        transactionQueue.clearBlockTransactions({
-          transactionSeries: block.transactionSeries
-        });
+          // const postTransact = (block) => {
+          //   return new Promise((resolve, reject) => {
+          //     request(
+          //       ,
+          //       {
+          //         method: "POST",
+          //         body: JSON.stringify(block),
+          //       },
+          //       (error, response, body) => {
+          //         return resolve(JSON.parse(body));
+          //       }
+          //     );
+          //   });
+          // };
+          // console.log(block);
+          // postTransact(block).then((postTransactResponse) => {
+          //   console.log("data upload to dashbaord");
+          // });
 
-        return resolve();
-      }).catch(reject);
+          request.post(
+            "http://localhost:1234/dashboardblock",
+            {
+              json: {
+                block,
+              },
+            },
+            (error, res, body) => {
+              if (error) {
+                console.error(error);
+                return;
+              }
+              console.log(`statusCode: ${res.statusCode}`);
+              console.log(body);
+            }
+          );
+
+          transactionQueue.clearBlockTransactions({
+            transactionSeries: block.transactionSeries,
+          });
+
+          return resolve();
+        })
+        .catch(reject);
     });
   }
 
   replaceChain({ chain }) {
     return new Promise(async (resolve, reject) => {
-      for (let i=0; i<chain.length; i++) {
+      for (let i = 0; i < chain.length; i++) {
         const block = chain[i];
-        const lastBlockIndex = i-1;
-        const lastBlock = lastBlockIndex >= 0 ? chain[i-1] : null;
+        const lastBlockIndex = i - 1;
+        const lastBlock = lastBlockIndex >= 0 ? chain[i - 1] : null;
 
         try {
           await Block.validateBlock({ lastBlock, block, state: this.state });
